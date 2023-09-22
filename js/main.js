89dbf7b28d453fbb1d40ebecaa3f43d7f6b0a07f
akintadetemi19@gmail.com <akintadetemi19@gmail.com>
2023-09-23 00:38:18 +0100

    more JS functionalities

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const FILE_INPUT = document.querySelector("#file")
 const AUDIO = document.querySelector("audio")
 const PLAY_BUTTON = document.querySelector("#audiobtn")
+const AUDIO_LIST = []
+const SEEK = document.querySelector('#seek')
 
 FILE_INPUT.onchange = () => {
 	if (FILE_INPUT.files.length === 0) {
@@ -10,18 +12,31 @@ FILE_INPUT.onchange = () => {
 		let file_loaded = FILE_INPUT.files[0]
 		fileReader.onload = (e) => {
 			AUDIO.src = e.target.result;
+			//document.body.innerHTML = e.target.result
 			PLAY_BUTTON.disabled = false;
+			SEEK.disabled = false;
 		}
 		fileReader.readAsDataURL(file_loaded);
 	}
+
 }
 
 PLAY_BUTTON.onclick = () => {
 	switch (PLAY_BUTTON.getAttribute('class')) {
 		case 'fa-solid fa-play':
 			PLAY_BUTTON.setAttribute('class', 'fa-solid fa-pause')
+			AUDIO.play()
 			break;
 		case 'fa-solid fa-pause':
 			PLAY_BUTTON.setAttribute('class', 'fa-solid fa-play')
+			AUDIO.pause()
 	}
+}
+
+AUDIO.onended = () => {
+	PLAY_BUTTON.setAttribute('class', 'fa-solid fa-play')
+}
+
+let createSeek = () => {
+	alert(AUDIO.duration)
 }
